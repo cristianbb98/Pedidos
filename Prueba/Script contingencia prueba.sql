158659d8bb7ccd693158918884b7c932301738b1
Cristian Betancourt <cristianbb98@gmail.com>
2019-06-05 22:58:14 -0500

    v1.2 Script contingencia prueba
    RESTAURAR ACTUALIZACIÓN DE VALORES DEL CAMPO CODPED DE CABEZERAP Y DETALLEP

--- Prueba/Script contingencia prueba.sql
+++ Prueba/Script contingencia prueba.sql
@@ -13,3 +13,45 @@ SET codped = 'R' + SUBSTRING(codped,DATALENGTH(codped)-1,2)
 UPDATE movimiento.DetalleP
 SET codped = 'R' + SUBSTRING(codped,DATALENGTH(codped)-1,2)
 GO
+
+	-- CREAR TABLAS AUXILIARES PARA CABEZERAP Y DETALLEP
+CREATE TABLE CabezeraPAux(
+	[codped] [char](3) NOT NULL,
+	[fecha] [datetime] NULL,
+	[montototal] [numeric](10, 2) NULL,
+	[codcli] [char](3) NOT NULL)
+
+CREATE TABLE DetallePAux(
+	[numlinea] [tinyint] NOT NULL,
+	[preciou] [numeric](7, 2) NULL,
+	[cantidad] [smallint] NULL,
+	[codped] [char](3) NOT NULL,
+	[codpro] [char](3) NOT NULL)
+
+	-- INSERTAR VALORES DE LA TABLA CABEZERAP Y DETALLEP EN
+	-- LAS TABLAS AUXILIARES CORRESPONDIENTES
+INSERT INTO CabezeraPAux 
+SELECT * FROM movimiento.CabezeraP
+
+INSERT INTO DetallePAux
+SELECT* FROM movimiento.DetalleP
+
+	-- ELIMINAR VALORES DE LA TABLA CABEZERAP Y DETALLEP
+DELETE FROM movimiento.CabezeraP
+DELETE FROM movimiento.DetalleP
+
+	-- REDUCIR TAMAÑO DEL CAMPO CODPED DE CABEZERAP Y DETALLEP
+ALTER TABLE movimiento.CabezeraP
+ALTER COLUMN codped CHAR(3)
+
+ALTER TABLE movimiento.DetalleP
+ALTER COLUMN codped CHAR(3)
+GO
+
+	-- MOVER DATOS DE TALBAS AUXILIARES A CABEZERAP Y DETALLEP
+	-- SEGÚN CORRESPONDA
+INSERT INTO movimiento.DetalleP
+SELECT * FROM DetallePAux
+
+INSERT INTO movimiento.CabezeraP
+SELECT * FROM CabezeraPAux
